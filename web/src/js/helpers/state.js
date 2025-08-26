f42eae7d51ef9568b2474604d0714323b1ba11a0
Pablo Werlang <pswerlang@gmail.com>
2025-08-25 23:15:07 -0300

    Add state manager.

---
+++ web/src/js/helpers/state.js
@@ -0,0 +1,44 @@
+import LocalData from "./local-data.js";
+
+export default class StateManager {
+
+    constructor({ key, defaults }={}) {
+        this.key = key ?? 'app-state';
+        this.defaults = defaults ?? {};
+        this.localData = new LocalData({
+            id: this.key,
+            data: this.defaults
+        });
+        this.state = null;
+        this.callbackList = [];
+        this.get();
+    }
+
+    get() {
+        if (!this.state) {
+            this.state = this.localData.get();
+        }
+        return this.state;
+    }
+
+    update(data) {
+        if (!this.state) {
+            this.get();
+        }
+        this.state = { ...this.state, ...data };
+        this.localData.set({ data: this.state });
+        this.callbackList.forEach(callback => callback(this.state));
+    }
+
+    onUpdate(callback) {
+        this.callbackList.push(callback);
+    }
+
+    clear() {
+        this.update(this.defaults);
+    }
+
+    alert() {
+        this.update({ ...this.get() });
+    }
+}
